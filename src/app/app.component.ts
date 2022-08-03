a6524780d6cc6b0d8eb9cb71fc1b9f2048d0e378
Christian Montoya <chrimonlo@gmail.com>
2022-08-03 00:05:16 -0500

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'Nicolas';
-  age = 18;
-  img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true;
+  name:string = 'Nicolas';
+  age:number = 18;
+  img:string = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled:boolean = true;
   person = {
     name: 'Nicolas',
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
-
+  names: string[] = ['Jesus', 'Jose', 'Amalia'];
+  newName = '';
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
@@ -32,5 +33,14 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+  
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+  
+  deleteName(index:number) {
+    this.names.splice(index, 1);
   }
 }
