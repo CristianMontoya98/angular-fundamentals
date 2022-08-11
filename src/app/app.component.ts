9aeaf0836f7d5f44ffe3c48782fcb0a9d6844264
Christian Montoya <chrimonlo@gmail.com>
2022-08-10 20:53:06 -0500

    Idented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,43 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name:string = 'Nicolas';
-  age:number = 18;
-  img:string = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled:boolean = true;
+  name: string = 'Nicolas';
+  age: number = 18;
+  img: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled: boolean = true;
   person = {
     name: 'Nicolas',
     age: 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-  }
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
   names: string[] = ['Jesus', 'Jose', 'Amalia'];
   newName = '';
   products: any[] = [
     {
       name: 'El mejor juguete',
       price: 500,
-      image: '../assets/img/toy.jpg'
+      image: '../assets/img/toy.jpg',
     },
     {
       name: 'Bicicleta casi nueva',
       price: 400,
-      image: '../assets/img/bike.jpg'
+      image: '../assets/img/bike.jpg',
     },
     {
       name: 'Colección de albumes',
       price: 500,
-      image: '../assets/img/album.jpg'
-    }
+      image: '../assets/img/album.jpg',
+    },
   ];
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
 
   increaseAge() {
-    this.person.age ++;
+    this.person.age++;
   }
 
   onScroll(event: Event) {
@@ -51,13 +51,13 @@ export class AppComponent {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
-  
+
   addName() {
     this.names.push(this.newName);
     this.newName = '';
   }
-  
-  deleteName(index:number) {
+
+  deleteName(index: number) {
     this.names.splice(index, 1);
   }
 }
